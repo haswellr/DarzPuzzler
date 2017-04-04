be999388696d339b3a7b27e0a688e9124c86f811
Ryan Haswell <haswellr@hotmail.com>
2017-04-03 17:14:05 -0700

    resolves #8 and also adjusts camera angle

---
+++ Darz/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Vector3 CAMERA_OFFSET;
+
+    private Character character;
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        character = this.gameObject.GetComponent<Character>();
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newCamPosition = mainCamera.transform.position;
+        newCamPosition.x = character.transform.position.x + CAMERA_OFFSET.x;
+        newCamPosition.z = character.transform.position.z + CAMERA_OFFSET.z;
+        mainCamera.transform.position = newCamPosition;
+	}
+}
